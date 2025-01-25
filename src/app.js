6d3ae4afc4e48a92715874519c77474d7a4a8cb0
Vedansh Mishra <vedanshm2001@gmail.com>
2025-01-26 00:25:54 +0530

    feat : routes in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
 
+import categoryRoutes from "./routes/CategoryRoutes.js";
+import subCategoryRoutes from "./routes/SubCategoryRoutes.js";
+import itemRoutes from "./routes/ItemRoutes.js";
+
 const app = express();
 
 //which origins should be able to acess your backend
@@ -14,5 +18,12 @@ app.use(
 app.use(express.json({limit: "16kb"})) //limit of JSON data
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
+
+
+app.use("/api/categories", categoryRoutes);
+app.use("/api/subcategories", subCategoryRoutes);
+app.use("/api/items", itemRoutes);
+
+
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 8001;
 
 connectDB()
 .then(() => {
+    console.log("connected to MongoDB");
+    
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`); 
     })

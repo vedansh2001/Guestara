525011ba7e874270f00a7d30c9f2a71cd613c0be
Vedansh Mishra <vedanshm2001@gmail.com>
2025-01-31 02:13:59 +0530

    fix : cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,10 +9,11 @@ const app = express();
 
 //which origins should be able to acess your backend
 app.use(cors({
-    origin: ['http://localhost:5173'], // Specific frontend origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
-  }));
+  origin: ['http://localhost:5173', 'https://guestara-crud.vercel.app'], // Allow local & deployed frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use(express.json({limit: "16kb"})) //limit of JSON data
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
